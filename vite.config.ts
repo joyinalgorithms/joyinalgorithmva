6a951c6484c132cb8223472f7d24e69e11f06a93
Joy <122730271+joyinalgorithms@users.noreply.github.com>
2025-11-05 09:31:32 +0000

    Wed, Nov 5, 2025, 5:31 PM +08:00

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/joyinalgorithmva/", 
+  base: "/joyinalgorithmva/",
   server: {
     host: "::",
     port: 8080,
@@ -15,5 +14,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
   },
 }));
